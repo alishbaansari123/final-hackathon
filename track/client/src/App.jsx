5a7cb1f6e791bf957ed2ccf296090e68f8684656
Your Name <you@example.com>
2025-05-08 22:26:26 +0500

    Your commit message

--- track/client/src/App.jsx
+++ track/client/src/App.jsx
@@ -1,40 +1,12 @@
-// import React from 'react';
-// import { TaskProvider } from './context/TaskContext';
-// import TaskBoard from './components/TaskBoard'; // <- import TaskBoard
-
-// function App() {
-//   return (
-//     <TaskProvider>
-//       <TaskBoard />
-//     </TaskProvider>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
 import { TaskProvider } from './context/TaskContext';
-import TaskBoard from './components/TaskBoard'; // tumhara tracking system
-import Auth from './Pages/Auth'; // <-- sirf ek file Auth.jsx
+import TaskBoard from './components/TaskBoard'; // <- import TaskBoard
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
-
   return (
-    <BrowserRouter>
-      <TaskProvider>
-        <Routes>
-          <Route path="/auth" element={<Auth setToken={setToken} />} />
-          <Route
-            path="/dashboard"
-            element={token ? <TaskBoard /> : <Navigate to="/auth" />}
-          />
-          <Route path="*" element={<Navigate to="/auth" />} />
-        </Routes>
-      </TaskProvider>
-    </BrowserRouter>
+    <TaskProvider>
+      <TaskBoard />
+    </TaskProvider>
   );
 }
 
